1325c075ca7374e33ab65fc46295781b3d91011a
Sean Christman <seanchristman381@gmail.com>
2023-02-13 02:30:49 -0600

    Module 8 Commit

--- movies_update_and_delete.py
+++ movies_update_and_delete.py
@@ -14,24 +14,25 @@ config = {
 # Connect to the movies database
 db = mysql.connector.connect(**config)
 
+# Declare show_films method
 def show_films(cursor, title):
     # method to execute an inner join on all tables,
     # iterate over the dataset and output the results to the terminal window.
 
-    # inner join query
+    # Create inner join query
     cursor.execute("Select film_name as Name, film_director as Director, genre_name as Genre, studio_name as 'Studio Name' from film INNER JOIN genre ON film.genre_id=genre.genre_id INNER JOIN studio ON film.studio_id=studio.studio_id")
 
-    # get the results from the cursor object
+    # Get the results from the cursor object
     films = cursor.fetchall()
 
     print("\n -- {} --".format(title))
 
-    # iterate over the film data set and display the results
+    # Iterate over the film data set and display the results
     for film in films:
         print("Film Name: {}\nDirector: {}\nGenre Name ID: {}\nStudio Name: {}\n".format(
             film[0], film[1], film[2], film[3]))
 
-# update method
+# Declare update method
 def update():
 
     # Create cursor
@@ -40,17 +41,17 @@ def update():
     # Displaying existing records in database
     show_films(cursor, "DISPLAYING FILMS")
 
-    # Perform Insertion of new movie
+    # Insert Star Wars into table
     cursor.execute("Insert into film(film_name, film_releaseDate, film_runtime, film_director, studio_id, genre_id) values ('Star Wars', '1977', '121', 'George Lucas', (SELECT studio_id FROM studio WHERE studio_name = '20th Century Fox'),(SELECT genre_id FROM genre WHERE genre_name = 'SciFi') );")
     db.commit()
     show_films(cursor, "DISPLAYING FILMS AFTER INSERT")
 
-    # Perform Update of Alien movie
+    # Update Alien genre to Horror
     cursor.execute("Update film set genre_id = (select genre_id from genre where genre_name = 'Horror') where film_name = 'Alien';")
     db.commit()
     show_films(cursor, "DISPLAYING FILMS AFTER UPDATE- Changed Alien to Horror")
 
-    # Delete Gladiator movie from table
+    # Delete Gladiator from table
     cursor.execute("Delete from film where film_name = 'Gladiator';")
     db.commit()
     show_films(cursor, "DISPLAYING FILMS AFTER DELETE")
